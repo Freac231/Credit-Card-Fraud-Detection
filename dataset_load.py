1a04d05bf3644902a03d7857f83249881eb6c758
Michael livshits <livshmic@post.bgu.ac.il>
2025-05-31 20:56:38 +0300

    minor changes

--- dataset_load.py
+++ dataset_load.py
@@ -1,8 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = ''
 
-if __name__ == "__main__":
-    path = kagglehub.dataset_download("joebeachcapital/credit-card-fraud")
-    print(f"Dataset downloaded: {path}")
+path = kagglehub.dataset_download("joebeachcapital/credit-card-fraud")
+print(f"Dataset downloaded: {path}")
